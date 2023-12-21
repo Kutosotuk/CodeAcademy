f04357e23de9f915ded676ff2b803e53a733dc55
Alexis Wallace <alexis-wallace@outlook.com>
2023-12-21 20:37:10 +1100

    added CF lesson - Lists

---
+++ Code Foundations/Lessons/3. Basics of Programming III/1. Lists/main.js
@@ -0,0 +1,6 @@
+// Build a list
+comicStrip = ['Codey sees the trail', 'Codey starts the hike', 'Codey is halfway', 'Codey reaches the finish']
+
+// select the 4th item from the list
+// and save it to the variable selection
+selection =mcomicStrip.splice(3, 0)
