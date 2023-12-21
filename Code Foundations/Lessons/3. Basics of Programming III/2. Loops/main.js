ec7135a59f899d44765e3bd7bf79054e98655dcc
Alexis Wallace <alexis-wallace@outlook.com>
2023-12-21 20:54:04 +1100

    added CF lesson - Loops

---
+++ Code Foundations/Lessons/3. Basics of Programming III/2. Loops/main.js
@@ -0,0 +1,17 @@
+// Copy code here and run it!
+
+// For Loop
+for (i = 0; i < 10; i++) {
+    placeTile('mint')
+    placeTile('orange')
+    placeTile('mint')
+}
+
+//While Loop
+let i = 0;
+while (i < 10) {
+    placeTile('mint')
+    placeTile('orange')
+    placeTile('mint')
+    i++
+}
